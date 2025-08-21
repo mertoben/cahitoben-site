56637ad483bd65a7e12a6ab3ea13037396e709a5
mertoben <mertoben@gmail.com>
2025-08-21 17:11:37 +0300

    Add files via upload

--- lib/instagram.ts
+++ lib/instagram.ts
@@ -1,7 +1,14 @@
 export const INSTAGRAM_POSTS: string[] = [
-  // Örnek (profilinizden tekil gönderi URL’leri):
-  // 'https://www.instagram.com/p/XXXXXXXXXXX/',
-  // 'https://www.instagram.com/p/YYYYYYYYYYY/',
-  // Not: Instagram, otomatik profil taramasını API’siz engeller.
-  // Buraya son 6-9 gönderinin URL’sini eklemeniz yeterli.
+  'https://www.instagram.com/p/DKMUplTIv4E/',
+  'https://www.instagram.com/p/DKFIkgsIrZe/',
+  'https://www.instagram.com/p/DHgCo7jKiwy/',
+  'https://www.instagram.com/p/DGcb3-QKrPO/',
+  'https://www.instagram.com/p/CUpn7joK0Tt/',
+  'https://www.instagram.com/p/CUnJR19KlHr/',
+  'https://www.instagram.com/p/CQdkE_-BjHD/',
+  'https://www.instagram.com/p/CQdhny_Bi3w/',
+  'https://www.instagram.com/p/CPdkhvQhlrW/',
+  'https://www.instagram.com/p/CO5BDY7MC51/',
+  'https://www.instagram.com/p/CO2t575sqNH/',
+  'https://www.instagram.com/p/CO2tugsMlJj/'
 ]
